51f9afb0e170ae75800df2a9e282d8ca401f645a
aditheawesome <adityamittal1207@gmail.com>
2026-05-31 22:00:23 -0700

    add firebase service acct info into env for railway deployment

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -6,25 +6,32 @@ import admin from "firebase-admin";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const defaultServiceAccountPath = path.join(__dirname, "../firebase-service-account.json");
 
-function resolveCredential() {
+function credentialFromEnv() {
+  const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = process.env;
+  if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
+    return null;
+  }
+  return admin.credential.cert({
+    projectId: FIREBASE_PROJECT_ID,
+    clientEmail: FIREBASE_CLIENT_EMAIL,
+    privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  });
+}
+
+function credentialFromFile() {
   const serviceAccountPath =
     process.env.FIREBASE_SERVICE_ACCOUNT_PATH || defaultServiceAccountPath;
 
-  if (existsSync(serviceAccountPath)) {
-    const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
-    return admin.credential.cert(serviceAccount);
+  if (!existsSync(serviceAccountPath)) {
+    return null;
   }
 
-  const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = process.env;
-  if (FIREBASE_PROJECT_ID && FIREBASE_CLIENT_EMAIL && FIREBASE_PRIVATE_KEY) {
-    return admin.credential.cert({
-      projectId: FIREBASE_PROJECT_ID,
-      clientEmail: FIREBASE_CLIENT_EMAIL,
-      privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    });
-  }
+  const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
+  return admin.credential.cert(serviceAccount);
+}
 
-  return null;
+function resolveCredential() {
+  return credentialFromEnv() ?? credentialFromFile();
 }
 
 const credential = resolveCredential();
